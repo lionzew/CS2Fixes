319c13de121622db75f6108600a70e68c10b0246
blu133721 <tiganinpungasrl@gmail.com>
2023-10-29 16:30:30 +0200

    Update events.cpp

--- src/events.cpp
+++ src/events.cpp
@@ -149,10 +149,6 @@ GAME_EVENT_F(player_spawn)
             pController->m_szClan("[Player]");
         }	
 
-		pPawn->m_pCollision->m_collisionAttribute().m_nCollisionGroup = COLLISION_GROUP_DEBRIS;
-		pPawn->m_pCollision->m_CollisionGroup = COLLISION_GROUP_DEBRIS;
-		pPawn->CollisionRulesChanged();
-
 		return -1.0f;
 	});
 }
