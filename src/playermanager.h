b81aaf65eccd65736c11cdc0ec1c20d6881640d9
blu133721 <tiganinpungasrl@gmail.com>
2023-10-31 12:25:34 +0200

    Changes

--- src/playermanager.h
+++ src/playermanager.h
@@ -49,6 +49,8 @@ public:
 		m_iHideDistance = 0;
 		m_bConnected = false;
 		m_iTotalDamage = 0;
+		m_bVotedRTV = false;
+		m_bVotedExtend = false;
 		m_bUsedMedkit = false;
 	}
 
@@ -70,6 +72,8 @@ public:
 	void ClearTransmit() { m_shouldTransmit.ClearAll(); }
 	void SetHideDistance(int distance) { m_iHideDistance = distance; }
 	void SetTotalDamage(int damage) { m_iTotalDamage = damage; }
+	void SetRTVVote(bool bRTVVote) { m_bVotedRTV = bRTVVote; }
+	void SetExtendVote(bool bExtendVote) { m_bVotedExtend = bExtendVote; }
 
 	bool IsMuted() { return m_bMuted; }
 	bool IsGagged() { return m_bGagged; }
@@ -77,6 +81,8 @@ public:
 	int GetHideDistance() { return m_iHideDistance; }
 	CPlayerSlot GetPlayerSlot() { return m_slot; }
 	int GetTotalDamage() { return m_iTotalDamage; }
+	bool GetRTVVote() { return m_bVotedRTV; }
+	bool GetExtendVote() { return m_bVotedExtend; }
 	
 	void OnAuthenticated();
 	void CheckAdmin();
@@ -98,6 +104,8 @@ private:
 	int m_iHideDistance;
 	CBitVec<MAXPLAYERS> m_shouldTransmit;
 	int m_iTotalDamage;
+	bool m_bVotedRTV;
+	bool m_bVotedExtend;
 };
 
 class CPlayerManager
